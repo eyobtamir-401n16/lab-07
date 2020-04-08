7974611a3ce31325f5f947cec573f773d1803ca5
Eyob1984 <tamireyob@gmail.com>
2020-04-07 20:17:22 -0700

    error function added

--- doc/swagger.js
+++ doc/swagger.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const app = express();
-const expressSwagger = require('express-swagger-generator')(app);
+const generateSwagger = (app) => {
+    const expressSwagger = require('express-swagger-generator')(app);
 
-let options = {
-    swaggerDefinition: {
-        info: {
-            description: 'This is a sample server',
-            title: 'Swagger',
-            version: '1.0.0',
+    let options = {
+        swaggerDefinition: {
+            info: {
+                description: 'Access CRUD operations for fruits and vegetables',
+                title: 'My API Server',
+                version: '1.0.0',
+            },
+            host: 'localhost:3000',
+            basePath: '/',
+            produces: ['application/json', 'text/html'],
+            schemes: ['http'],
+            securityDefinitions: {
+                JWT: {
+                    type: 'apiKey',
+                    in: 'header',
+                    name: 'Authorization',
+                    description: '',
+                },
+            },
         },
-        host: 'localhost:3000',
-        basePath: '/',
-        produces: [
-            "application/json",
-            "text/html"
-        ],
-        schemes: ['http', 'https'],
-		securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
-    },
-    basedir: __dirname, //app absolute path
-    files: ['../lib/server.js'] //Path to the API handle folder
+        basedir: __dirname, //app absolute path
+        files: ['../lib/server.js'], //Path to the API handlers
+    };
+
+    expressSwagger(options);
 };
-expressSwagger(options)
-app.listen(3001);
+
+module.exports = generateSwagger;
